9de675db7fb85447a78f0e35ca809b66a0597023
Johnny Gill <swfiua@gmail.com>
2017-07-04 11:53:56 -0300

    oh canada

---
+++ oc.py
@@ -0,0 +1,13 @@
+import music
+
+oh_canada = ['g:4',  'a:3', 'a',  'e:5', 
+    'f', 'g', 'a', 'b', 'c', 'f',
+    'g:4', 'a#', 'c', 'b:5', 'c', 'd', 'd', 'c' , 'c', 'b']
+
+oh_canada2 = ['e', 'g', 'g', 'c']
+
+oc = 'eggcdefgad' + 'ef'
+
+oc = [x for x in oc]
+
+music.play(oc)
